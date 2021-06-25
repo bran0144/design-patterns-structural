ad9d4f4d1d9397255aeb7a988d1bd2b9b2cebe57
bra0144 <bran0144@umn.edu>
2021-06-25 12:16:45 -0500

    simple adapter pattern

---
+++ src/com/pluralsight/adapter/AdapterPattern.java
@@ -0,0 +1,18 @@
+package com.pluralsight.adapter;
+// Good for connecting new code to legacy code
+// Converts interface into another interface (translates requests)
+// Client centric
+// Examples: Arrays -> Lists
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AdapterPattern {
+
+    public static void main(String args[]) {
+        Integer[] arrayOfInts = new Integer[]{42, 43, 44};
+        List<Integer> listOfInts = Arrays.asList(arrayOfInts);
+        System.out.println(arrayOfInts);
+        System.out.println(listOfInts);
+    }
+}
