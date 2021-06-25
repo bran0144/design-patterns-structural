407d7aa0d2f80057a01b9986fb054c0122aca62f
bra0144 <bran0144@umn.edu>
2021-06-25 15:32:50 -0500

    adding a new bridge class (new formatter)

--- src/com/pluralsight/bridge/BridgeMovieDemo.java
+++ src/com/pluralsight/bridge/BridgeMovieDemo.java
@@ -14,5 +14,9 @@ public class BridgeMovieDemo {
 
         String printedMaterial = moviePrinter.print(printFormatter);
         System.out.println(printedMaterial);
+
+        Formatter htmlFormatter = new HtmlFormatter();
+        String htmlMaterial = moviePrinter.print(htmlFormatter);
+        System.out.println(htmlMaterial);
     }
 }
